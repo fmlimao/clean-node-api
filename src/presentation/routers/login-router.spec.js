19fa5136a45a3f8c3cc18e53245a2a40f9d05713
Leandro Macedo <fmlimao@gmail.com>
2021-08-21 19:50:59 -0300

    Garantindo que o LoginRouter retorne 500 se ele receber um httpRequest inválido

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,13 @@
 class LoginRouter {
   route (httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500
+      }
+    }
+
     const { email, password } = httpRequest.body
+
     if (!email || !password) {
       return {
         statusCode: 400
@@ -29,12 +36,28 @@ describe('Login Router', () => {
 
     const httpRequest = {
       body: {
-        email: 'any_email@email'
+        email: 'any_email@email.com'
       }
     }
 
     const httpResponse = sut.route(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Deve retornar 500 se o httpRequest não for enviado', () => {
+    const sut = new LoginRouter()
+
+    const httpResponse = sut.route()
+
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Deve retornar 500 se o httpRequest não tiver body', () => {
+    const sut = new LoginRouter()
+
+    const httpResponse = sut.route({})
+
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
