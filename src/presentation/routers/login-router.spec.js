b86bdf5c4f8933c1e6a59b887ff10335327ccde4
Leandro Macedo <fmlimao@gmail.com>
2021-08-21 19:31:44 -0300

    Garantindo que o LoginRouter retorne 400 se o email não for enviado

---
+++ src/presentation/routers/login-router.spec.js
@@ -0,0 +1,25 @@
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login Router', () => {
+  test('Deve retornar 400 se nenhum email for enviado', () => {
+    const sut = new LoginRouter()
+
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
